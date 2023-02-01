14924d7286b8e55506035252788aff76faad85fd
kevinharr <kevinharr@verizon.net>
2023-01-31 22:58:34 -0500

    add Contact link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
       <img
       src="/images/Facebook_Photo_San_Fran.jpg"
       style={{ width: "300px", height: "300px"}}
-      alt="Kevin Harrington Picture infront of Golden Gate Bridge"
+      alt="Kevin Harrington infront of Golden Gate Bridge"
       />
       <p style={{ width: "599px"}}>Kevin Harrington recently retired from Prudential as Vice President of Product Management in Group Insurance.  He is currently attending a Software Engineering Immersive with General Assembly.<br/><br/>  Upon graduating, Kevin intends to leverage his 30 plus years experience in Group Insurance, training and product management to help enhance the corporate benefits space using current technology.</p>
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,15 @@
+
+
 function Contact() {
 	return (
-    <h1>Contact</h1>
+    <div>
+      <h1>Contact</h1>
+      <p>If you would like to collaborate on an interesting project, feel free to reach out to me via <a href = "mailto: kevinharr@verizon.net">email</a>.<br/><br/>Reach out to me on social media!</p>
+      <ul>
+        <li><a href = "www.linkedin.com/in/kevin-p-harrington">LinkedIn</a></li>
+      </ul>
+    </div>
+
   )
 }
 
