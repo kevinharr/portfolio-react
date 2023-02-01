07164e1e48dc5b294e70cb8725a786e11d95506f
kevinharr <kevinharr@verizon.net>
2023-02-01 08:20:14 -0500

    fix resume link

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,12 @@
 function Resume() {
 	return (
+    <div>
     <h1>Resume</h1>
+    <p>Click the link to download my <a href="/Kevin_Harrington_Resume_1_9_2023.pdf" download>
+      resume</a>.</p>
+
+    </div>
+   
   )
 }
   
