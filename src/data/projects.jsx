50a7e6e05fe78204a9989ccc7b53519f53c68c25
kevinharr <kevinharr@verizon.net>
2023-02-01 08:55:17 -0500

    setup initial data set

---
+++ src/data/projects.jsx
@@ -0,0 +1,6 @@
+export const projects = [
+    { id: 62345, title: 'BirdBux', description: 'Trivia game covering Movies, Music, Travel and Art', image: 'Club', repositoryLink: 'Club', deploymentLink: 'Club', },
+    { id: 22345, title: 'Soundboard', description: 'Club', image: 'Club', repositoryLink: 'Club', deploymentLink: 'Club', },
+  ]
+  
+  
