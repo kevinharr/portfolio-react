ac3f1ad517d85f4a469c18f814b5a0b0ae2ea62d
kevinharr <kevinharr@verizon.net>
2023-02-01 07:29:37 -0500

    fix LinkedIn link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
       <h1>Contact</h1>
       <p>If you would like to collaborate on an interesting project, feel free to reach out to me via <a href = "mailto: kevinharr@verizon.net">email</a>.<br/><br/>Reach out to me on social media!</p>
       <ul>
-        <li><a href = "www.linkedin.com/in/kevin-p-harrington">LinkedIn</a></li>
+        <li><a href = "https://linkedin.com/in/kevin-p-harrington">LinkedIn</a></li>
       </ul>
     </div>
 
