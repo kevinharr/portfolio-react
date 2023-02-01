ca5f622a5521634c4217bb3a527b3e8b875a7f4d
kevinharr <kevinharr@verizon.net>
2023-01-31 22:09:58 -0500

    create Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
 function Home() {
 	return (
-    <h1>Home</h1>
+    <div>
+      <h1>Kevin Harrington</h1>
+      <h2>Software Engineer</h2>
+      <img
+      src="/images/Facebook_Photo_San_Fran.jpg"
+      style={{ width: "300px", height: "300px"}}
+      alt="Kevin Harrington Picture infront of Golden Gate Bridge"
+      />
+      <h3 style={{ width: "330px"}}>Leveraging experience in product, training and tech to improve Group insurance.</h3>
+    </div>
+  
+
   )
 }
   
