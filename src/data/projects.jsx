54b54a15f00771ba0bde17961827d69bcd25747d
kevinharr <kevinharr@verizon.net>
2023-02-01 19:46:04 -0500

    setup data

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,6 +1,6 @@
 export const projects = [
-    { id: 62345, title: 'BirdBux', description: 'Trivia game covering Movies, Music, Travel and Art', image: 'Club', repositoryLink: 'Club', deploymentLink: 'Club', },
-    { id: 22345, title: 'Soundboard', description: 'Club', image: 'Club', repositoryLink: 'Club', deploymentLink: 'Club', },
+    { id: 62345, title: 'BirdBux', description: 'Trivia game covering Movies, Music, Travel and Art', image: '/images/Bird_Bux_Screenshot.png', repositoryLink: 'https://github.com/kevinharr/bird-bux.git', deploymentLink: 'https://cute-crisp-5f7544.netlify.app'},
+    { id: 22345, title: 'Soundboard', description: 'A database that keeps track of your favorite artist and if they are in concert.', image: '/images/Soundboard_Splash_Page.jpg', repositoryLink: 'https://github.com/kevinharr/song-collector.git', deploymentLink: 'https://song-collector.fly.dev'},
   ]
   
   
